609e53327a3c0a0af9d2d34a23e54c934b8185ad
adriapadilla <adrian.padilla.m@gmail.com>
2020-04-20 16:06:41 +0200

    add setup.py and uploaded to Pipy

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(name='imdb_films_scraper',
+      version="1.0",
+      url = "https://github.com/AdriaPadilla/imdb_films_scraper/", 
+      description='Scrape Film data from IMDB',
+      author='Adrian Padilla',
+      author_email='adrian.padilla.m@gmail.com',
+      packages=setuptools.find_packages(),
+      install_requires=["numpy", "pandas", "requests", "beautifulsoup4", "openpyxl", "tqdm"],
+      classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+       ],
+     )
